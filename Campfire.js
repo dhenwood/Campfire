47df5ec1a9c4b309ea44f0822e20edf7823c4c22
dhenwood <dhenwood@users.noreply.github.com>
2026-06-27 15:05:08 +1000

    Update Campfire.js

--- Campfire.js
+++ Campfire.js
@@ -1,13 +1,21 @@
 import xapi from 'xapi';
 
+// Written by Darren Henwood (dhenwood@cisco.com)
+//For details, see: https://github.com/dhenwood/Campfire
+
+// Update the following 5 values
 let botToken = "replaceWithBotToken"
+let codecDeviceId1 = "replaceWithDeviceIdCodec1"
 let codecDeviceId2 = "replaceWithDeviceIdCodec2"
 let codecDeviceId3 = "replaceWithDeviceIdCodec3"
 let codecDeviceId4 = "replaceWithDeviceIdCodec4"
 
-var remoteRequest = false // do not change
+let allCodecs = [codecDeviceId1, codecDeviceId2, codecDeviceId3, codecDeviceId4]
+var remainingCodecs // after init(), becomes an array of the other 3 codecs
+var remoteRequest = false // used to track if request is local or from remote codec
 
 function postMessage(codecDeviceId, requestType, dialNumber, displayName) {
+  console.log("tx request: " + requestType)
   let headerAuth = "Authorization: Bearer " + botToken
   let headerContent = "Content-Type: application/json"
   let url = "https://webexapis.com/v1/xapi/command/UserInterface.Message.Prompt.Display"
@@ -36,6 +44,7 @@ function postMessage(codecDeviceId, requestType, dialNumber, displayName) {
     }
   };
   let json = JSON.stringify(body)
+  console.log("tx jsonMsg: " + json)
 
   xapi.Command.HttpClient.Post({Header: [headerAuth, headerContent], Timeout: 10, Url: url}, json);
 }
@@ -46,9 +55,10 @@ async function getCallStatus(){
       let number = jsonParse.CallbackNumber
       let sipAddress = number.split(":");
       let displayName = jsonParse.DisplayName
-      postMessage(codecDeviceId2, "placeCall", sipAddress[1], displayName)
-      postMessage(codecDeviceId3, "placeCall", sipAddress[1], displayName)
-      postMessage(codecDeviceId4, "placeCall", sipAddress[1], displayName)
+      console.log(`Call to ${displayName} on number ${sipAddress[1]}`)
+      for (const eachCodec of remainingCodecs) {
+        postMessage(eachCodec, "placeCall", sipAddress[1], displayName)
+      }
   })
 }
 
@@ -61,16 +71,23 @@ function placeCall(value){
 
 function init(){
   xapi.Config.HttpClient.Mode.set("On");
+
+  xapi.Status.Webex.DeveloperId.get().then(thisDeviceId => {
+    remainingCodecs = allCodecs.filter(codec => codec !== thisDeviceId);
+    console.log("remainingCodecs: " + remainingCodecs)
+  })
+
   xapi.Status.SystemUnit.State.NumberOfActiveCalls.on(numberOfCalls => {
+    console.log("numberOfCalls: " + numberOfCalls)
     if (numberOfCalls == 1){
       getCallStatus()
     }else if (numberOfCalls == 0){
       if (remoteRequest == true){
         remoteRequest = false
       }else{
-        postMessage(codecDeviceId2, "endCall")
-        postMessage(codecDeviceId3, "endCall")
-        postMessage(codecDeviceId4, "endCall")
+        for (const eachCodec of remainingCodecs) {
+          postMessage(eachCodec, "endCall")
+        }
       }
     }else{
       console.log("call count: " + numberOfCalls)
@@ -78,19 +95,19 @@ function init(){
   })
 
   xapi.Status.Audio.Microphones.Mute.on(muteStatus => {
-    postMessage(codecDeviceId2, "muteCall" + muteStatus)
-    postMessage(codecDeviceId3, "muteCall" + muteStatus)
-    postMessage(codecDeviceId4, "muteCall" + muteStatus)
+    console.log("rx local muteStatus: " + muteStatus)
+    for (const eachCodec of remainingCodecs) {
+      postMessage(eachCodec, "muteCall" + muteStatus)
+    }
   });
 
   xapi.event.on('UserInterface Message Prompt Display', (messageContent) => {
+    console.log("rx remote msg: " + messageContent)
     if (messageContent.Title == "Placing Call") {
       placeCall(messageContent)
-      
     }else if (messageContent.Title == "Ending Call") {
       remoteRequest = true
       xapi.Command.Call.Disconnect({});
-      
     }else if (messageContent.Title == "Toggling Mute") {
       if (messageContent.Text == "Muting Audio"){
         xapi.Command.Audio.Microphones.Mute();
